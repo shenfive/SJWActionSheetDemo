9aabb234d19e7169871ab766bde58c301a039e02
Shen Jun Wu <shenfive@gmail.com>
2022-07-22 16:27:33 +0800

    first test

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SJWActionSheetDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'MaterialComponents/ActionSheet'
+  pod 'MaterialComponents/Dialogs'
+  # Pods for SJWActionSheetDemo
+
+end
--- SJWActionSheetDemo/ViewController.swift
+++ SJWActionSheetDemo/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import MaterialComponents.MaterialActionSheet
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         // Do any additional setup after loading the view.
+        let actionSheet = MDCActionSheetController(title: "Action Sheet",
+                                                   message: "Secondary line text")
+        
+        let actionOne = MDCActionSheetAction(title: "Home", image: nil) { action in
+            print("Home action")
+        }
+        let actionTwo = MDCActionSheetAction(title: "Email", image: nil) { action in
+            print("Email action")
+        }
+
+        actionSheet.addAction(actionOne)
+        actionSheet.addAction(actionTwo)
+
+        present(actionSheet, animated: true, completion: nil)
     }
 
 
